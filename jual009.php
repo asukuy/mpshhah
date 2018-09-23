e704d55c02568b916cf638e42fc65263d818109c
asukuy <39807161+asukuy@users.noreply.github.com>
2018-09-23 11:15:32 +0700

    Create jual009.php

---
+++ jual009.php
@@ -0,0 +1,104 @@
+<?php
+set_time_limit(100);
+error_reporting(0);
+ignore_user_abort(1);
+session_start();
+require_once('fungsi.php');
+$TimeZone="+7";
+$_time=gmdate("H", time() + ($TimeZone * 60 * 60));
+
+$_SESSION['data'] = array('cookies' => 'ds_user=jualfollowers_indo009;rur=FRC;mid=W6cTJQABAAH5Ucm4xr8STm7NoA-E;ds_user_id=2535286616;urlgen="{}:1g3vmv:FCtlGdnSL5PQcZ6GaFXVL10EnJk";sessionid=IGSC701028364401ca6ad3c951a6de1a50542f527b0b293ae7c1451c6446a7e58433%3ALeJMfQNcdaPGR21vbb4qd0oVa3GwL8SL%3A%7B%22_auth_user_id%22%3A2535286616%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A1%2C%22_token_ver%22%3A2%2C%22_token%22%3A%222535286616%3AEwD4bTVFngl65mZN5HpNZdRyPCVLLD1C%3A69a937d64526dd47fcc30898de59208ab2e68209ddc0c94ba9bac0801bc2d267%22%2C%22last_refreshed%22%3A1537676069.9282913208%7D;mcd=3;csrftoken=boJE4deTezsevI00dVGAgfUezsBNs6FR;', 'useragent' => 'Instagram 6.22.0 Android (11/1.3.1; 160; 768x1024; samsung; GT-I9100; GT-I9100; smdkc210; en_US)', 'device_id' => 'android-537de305e941fccdbba5627e3eefbb247', 'username' => 'jualfollowers_indo009', 'id' => '2535286616');
+while(true){
+	if($_time>6){
+      $jumlah= "20";
+        $_POST['tipe'] = "followers";
+        $target = "818333280";
+	$data_session = $_SESSION['data'];
+	$getinfo = proccess(1, $data_session['useragent'], 'users/'.$target.'/info/');
+	$getinfo = json_decode($getinfo[1]);
+	if($_POST['tipe']=='followers'):
+		if(!is_numeric($jumlah))
+			$limit = 1;
+		elseif($jumlah>($getinfo->user->follower_count-1))
+			$limit = $getinfo->user->follower_count-1;
+		else
+			$limit = $jumlah-1;
+		$tipe = 'followers';
+	else:
+		if(!is_numeric($jumlah))
+			$limit = 1;
+		elseif($jumlah>($getinfo->user->follower_count-1))
+			$limit = $getinfo->user->follower_count-1;
+		else
+			$limit = $jumlah-1;
+		$tipe = 'followers';
+	endif;
+	$c = 0;
+	$listids = array();
+	do{
+		$parameters = ($c>0) ? '?max_id='.$c : '';
+		$req = proccess(1, $data_session['useragent'], 'friendships/'.$target.'/'.$tipe.'/'.$parameters, $data_session['cookies']);
+		$req = json_decode($req[1]);
+		for($i=0;$i<count($req->users);$i++):
+			if(count($listids)<=$limit)
+				$listids[count($listids)] = $req->users[$i]->pk;
+		endfor;
+		$c = (isset($req->next_max_id)) ? $req->next_max_id : 0;
+	}while(count($listids)<=$limit);
+	for($i=0;$i<count($listids);$i++):
+			$cross = proccess(1, $data_session['useragent'], 'friendships/create/'.$listids[$i].'/', $data_session['cookies'], hook('{"user_id":"'.$listids[$i].'"}'));
+			$cross = json_decode($cross[1]);
+			print $i.'. <b>@'.$data_session['username'].' Follow => '.$listids[$i].PHP_EOL;
+			flush();
+      sleep(60);
+	endfor;
+}
+else
+{
+        $jumlah= "200";
+        $_POST['tipe'] = "following";
+	$target = $_SESSION['data']['id'];
+	$data_session = $_SESSION['data'];
+	$getinfo = proccess(1, $data_session['useragent'], 'users/'.$target.'/info/');
+	$getinfo = json_decode($getinfo[1]);
+	if($_POST['tipe']=='following'):
+		if(!is_numeric($jumlah))
+			$limit = 1;
+		elseif($jumlah>($getinfo->user->following_count-1))
+			$limit = $getinfo->user->following_count-1;
+		else
+			$limit = $jumlah-1;
+		$tipe = 'following';
+	else:
+		if(!is_numeric($jumlah))
+			$limit = 1;
+		elseif($jumlah>($getinfo->user->follower_count-1))
+			$limit = $getinfo->user->follower_count-1;
+		else
+			$limit = $jumlah-1;
+		$tipe = 'followers';
+	endif;
+	$c = 0;
+	$listids = array();
+	do{
+		$parameters = ($c>0) ? '?max_id='.$c : '';
+		$req = proccess(1, $data_session['useragent'], 'friendships/'.$target.'/'.$tipe.'/'.$parameters, $data_session['cookies']);
+		$req = json_decode($req[1]);
+		for($i=0;$i<count($req->users);$i++):
+			if(count($listids)<=$limit)
+				$listids[count($listids)] = $req->users[$i]->pk;
+		endfor;
+		$c = (isset($req->next_max_id)) ? $req->next_max_id : 0;
+	}while(count($listids)<=$limit);
+	for($i=0;$i<count($listids);$i++):
+			$cross = proccess(1, $data_session['useragent'], 'friendships/destroy/'.$listids[$i].'/', $data_session['cookies'], hook('{"user_id":"'.$listids[$i].'"}'));
+			$cross = json_decode($cross[1]);
+			print $i.'. <b>@'.$data_session['username'].'</b> <font color="green">Sukses Follow => </font><b style="color:gray;">[ @'.$listids[$i].' ]</b><br>';
+			flush();
+	endfor;
+}
+	
+	
+	
+}
+?>
