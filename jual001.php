0b1fc142e19f6f61d3ebf8fc15b48c2cdbd556ad
asukuy <39807161+asukuy@users.noreply.github.com>
2018-10-06 21:52:48 +0700

    Update jual001.php

--- jual001.php
+++ jual001.php
@@ -8,7 +8,7 @@ $TimeZone="+7";
 $_time=gmdate("H", time() + ($TimeZone * 60 * 60));
 
 $_SESSION['data'] = array('cookies' => 'ds_user=jualfollowers_indo003;shbid=18600;shbts=1537674600.255159;rur=FRC;mid=W6cNZwABAAFZ8zj6th3wkIjcA7If;ds_user_id=3176983775;urlgen="{}:1g3vPE:cAq2UP8CuYbcjHXjZcaaxTbfzOI";sessionid=IGSC29df10fa2dd00b96edaefeee4e6ffa66f98e424d715988c81b9327b95235b3b8%3AIdKKHf9k7aVPNAzuhxdFBGMaEhDsA7rm%3A%7B%22_auth_user_id%22%3A3176983775%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A1%2C%22_token_ver%22%3A2%2C%22_token%22%3A%223176983775%3A2vLWLxC2nbOKH0Mp279bsOQWDhjo45Qy%3A54e53ee1aba67b5f4a60c3bf96e3253b1fc4bef687eef12a80c880e842164f69%22%2C%22last_refreshed%22%3A1537674600.2646944523%7D;mcd=3;csrftoken=NHu0TgRrahYv1k34dGNFQDAHFR3V9mbk;', 'useragent' => 'Instagram 6.22.0 Android (10/1.4.0; 160; 320x480; samsung; SM-N9000; SM-N9000; smdkc210; en_US)', 'device_id' => 'android-f1903f234d3ba4da38a18aa25751457d2', 'username' => 'jualfollowers_indo003', 'id' => '3176983775');
-
+$jml = 0;
 while(true){
 	if($_time>6){
       $jumlah= "20";
@@ -49,9 +49,10 @@ while(true){
 	for($i=0;$i<count($listids);$i++):
 			$cross = proccess(1, $data_session['useragent'], 'friendships/create/'.$listids[$i].'/', $data_session['cookies'], hook('{"user_id":"'.$listids[$i].'"}'));
 			$cross = json_decode($cross[1]);
-			print $i.'. <b>@'.$data_session['username'].' Follow => '.$listids[$i].PHP_EOL;
+			print ++$jml.'. <b>@'.$data_session['username'].' Follow => '.$listids[$i].PHP_EOL;
 			flush();
-      sleep(60);
+		$rand = [97,101,103,93,107,111,113,117];
+     		sleep($rand[rand(0,7)]);
 	endfor;
 }
 else
