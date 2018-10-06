1215f38d2704065478c917556cb659aee815da33
asukuy <39807161+asukuy@users.noreply.github.com>
2018-10-06 22:04:43 +0700

    Update jual008.php

--- jual008.php
+++ jual008.php
@@ -10,7 +10,7 @@ $jml = 0;
 $_SESSION['data'] = array('cookies' => 'ds_user=jualfollowers_indo008;shbid=18600;shbts=1537675823.1577463;rur=PRN;mid=W6cSLgABAAE5RZqJmUOWWfTk1jZ8;ds_user_id=3064940313;urlgen="{}:1g3vix:H-QXQ1bGcSYRpLWOjazuGD-6lzw";sessionid=IGSC74dd11a9871d3849a4a0c18f3184e32003f7ad7ff69019503a62ab3fcf0b26b2%3AhWQCUaCx9YUzkdzj8I4scy9xKnk8DNTP%3A%7B%22_auth_user_id%22%3A3064940313%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A1%2C%22_token_ver%22%3A2%2C%22_token%22%3A%223064940313%3AUnU15o4mmwPXXvaf2q2gRsmXUCUtywFL%3A441bd68272bebe14fdfe207c603c005ae904a9743321183dc30213d262adcbf7%22%2C%22last_refreshed%22%3A1537675823.1662318707%7D;mcd=3;csrftoken=XuX1El4HVDmlGVJEzgUrAKCEtUsKqJh9;', 'useragent' => 'Instagram 6.22.0 Android (11/1.5.5; 320; 1024x768; samsung; SM-N9000; SM-N9000; smdkc210; en_US)', 'device_id' => 'android-0fd600c953cde8121262e322ef09f70e8', 'username' => 'jualfollowers_indo008', 'id' => '3064940313');
 while(true){
 	if($_time>6){
-      $jumlah= "20";
+      $jumlah= "3";
         $_POST['tipe'] = "followers";
         $target = "3261799";
 	$data_session = $_SESSION['data'];
