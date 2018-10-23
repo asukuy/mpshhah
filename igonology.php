f3170d747a7fcf1043def2149425e1f27989da28
asukuy <39807161+asukuy@users.noreply.github.com>
2018-10-23 15:04:13 +0700

    Update igonology.php

--- igonology.php
+++ igonology.php
@@ -8,7 +8,7 @@ $TimeZone="+7";
 $_time=gmdate("H", time() + ($TimeZone * 60 * 60));
 $_SESSION['data'] = array('cookies' => 'ds_user=igonology;shbid=18600;shbts=1538104841.8254995;rur=ATN;mid=W62eCQABAAHjzg2ASgeYRmothQWp;ds_user_id=7350629322;urlgen="{}:1g5jKb:Mvszm2yPpL4lOc5pDwbgUC3UULM";sessionid=IGSCaf32f7e4c9f2d2ffd44fdc8428abe77663c3a91c6a2418fe1d30bede5fbd9a0c%3A8gS2o8FZmvgsLEcLcMLJGyubSKV2IZIQ%3A%7B%22_auth_user_id%22%3A7350629322%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A1%2C%22_token_ver%22%3A2%2C%22_token%22%3A%227350629322%3Agp87UhFssM6lzetgwyXTxFHctKFH9oGv%3A25f5c773b1c1834a8fd90fb8672f713fef47905535a6380f69de606f2d0c348b%22%2C%22last_refreshed%22%3A1538104841.8409891129%7D;mcd=3;csrftoken=8uaccWHeKJ94MSBftLoOfxc8kDy6LSKP;', 'useragent' => 'Instagram 6.22.0 Android (10/2.4.2; 120; 480x320; samsung; GT-I9100; GT-I9100; smdkc210; en_US)', 'device_id' => 'android-30cde89544caa549a813d660c4b279676', 'username' => 'igonology', 'id' => '7350629322');
 while(true){
-	if($_time>6){
+	if($_time<6){
       $jumlah= "20";
         $_POST['tipe'] = "followers";
         $target = "342582658";
